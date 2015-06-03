2b3a712606503c3a713cf3899506b3d6d5e041c9
Sander Peerna <sanderpeerna@gmail.com>
2015-06-02 18:12:28 -0700

    Initial Commit

---
+++ HypnoNerd/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  HypnoNerd
+//
+//  Created by Sander Peerna on 6/2/15.
+//  Copyright (c) 2015 Sander Peerna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
